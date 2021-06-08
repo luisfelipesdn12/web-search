533419d50cc6a4353e690ae460b66ce099a2a7fc
Luis Felipe <luisfelipesdn12@gmail.com>
2021-06-07 22:10:02 -0300

    Add yarn prepublish

--- .bilirc.ts
+++ .bilirc.ts
@@ -10,7 +10,7 @@ const configuration: Configuration = {
             if (format === "commonjs") return "web-search.js";
             return defaultFileName;
         },
-    }
+    },
 };
 
 export default configuration;
