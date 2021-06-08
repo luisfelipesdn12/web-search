1c52a1f8e4e60450eae90881edd117b02dab93a9
Luis Felipe <luisfelipesdn12@gmail.com>
2021-06-07 22:02:06 -0300

    Change dist config

--- .bilirc.ts
+++ .bilirc.ts
@@ -3,8 +3,7 @@ import { Config as Configuration } from "bili";
 const configuration: Configuration = {
     input: "src/web-search.ts",
     output: {
-        format: ["es", "esm", "cjs", "commonjs", "umd", "umd-min"],
-        moduleName: "WebSearch",
+        format: ["es", "esm", "cjs", "commonjs"],
         fileName: ({ format }, defaultFileName) => {
             if (format === "es") return "web-search.mjs";
             if (format === "cjs") return "web-search.cjs";
